36785371917f1f5b7096ff46ec79f8ef6ccf4d17
agres <agres@DESKTOP-N8AD6A5>
2017-02-10 18:03:33 +0100

    Corregidas rutas

--- src/main/java/exec/main.java
+++ src/main/java/exec/main.java
@@ -1,15 +1,24 @@
 package exec;
 
+import java.io.File;
+
 import util.HephUtil;
 
 public class main {
 	
 	private static final String rutaProyecto = "\\Proyecto\\";
-	private static final String rutaClases = ".\\tests\\";
+	private static final String rutaClases = "\\Clases\\";
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		HephUtil.genera(rutaProyecto, rutaClases);
+		try {
+			File f = new File(".");
+			System.out.println(f.getCanonicalPath());
+			// TODO Auto-generated method stub
+			HephUtil.genera(f.getCanonicalPath()+rutaProyecto,f.getCanonicalPath()+ rutaClases);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 }
